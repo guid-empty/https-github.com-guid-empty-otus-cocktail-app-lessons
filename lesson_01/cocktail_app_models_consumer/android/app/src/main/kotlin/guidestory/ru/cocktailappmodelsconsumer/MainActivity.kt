59c4d56147352d85d6b0fc8bc9946d0bf13f6ab6
howker <howker2007@gmail.com>
2020-10-24 13:55:54 +0400

    2

--- lesson_01/cocktail_app_models_consumer/android/app/src/main/kotlin/guidestory/ru/cocktailappmodelsconsumer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package guidestory.ru.cocktailappmodelsconsumer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lesson_02/ex01_hello_world/android/app/src/main/kotlin/guidestory/ru/ex01helloworld/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package guidestory.ru.ex01helloworld
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
