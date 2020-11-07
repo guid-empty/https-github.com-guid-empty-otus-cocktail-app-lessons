0632c354ecf0efa73bf73928809060c3bbfbe72f
Konstantin Kuzminov <zal1358@gmail.com>
2020-11-07 13:56:08 +0200

    konstantin kuzminov hw03 lesson07

--- lesson_05/lesson_05/lib/main.dart
+++ lesson_05/lesson_05/lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:lesson_05/slivers/sliver_flexible_space.dart';
 
-
-
 //списки: ListView. GridView. PageView. CustomScroll;
 //SliverGridView. SliverListView. FlexibleSpace.SliverAppBar.
 
 void main() {
   runApp(MaterialApp(
     theme: ThemeData(
-      appBarTheme: AppBarTheme(
-        textTheme: TextTheme(
-          button: TextStyle(color: Colors.white)
-        )
-      )
-    ),
+        appBarTheme: AppBarTheme(
+            textTheme: TextTheme(button: TextStyle(color: Colors.white)))),
     home: SliversFlexibleSamplePage(),
   ));
 }
