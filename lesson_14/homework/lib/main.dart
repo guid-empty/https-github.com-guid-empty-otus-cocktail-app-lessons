d6bd7dfe44feb7f12d38fe534da76a758cf9a1a7
howker <howker2007@gmail.com>
2020-11-25 20:28:19 +0400

    task added

---
+++ lesson_14/homework/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:homework/ui/favourite_cocktails_screen.dart';
+import 'package:homework/ui/style/theme.dart';
+
+import 'core/src/repository/async_cocktail_repository.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: mainThemeData,
+      themeMode: ThemeMode.dark,
+      home: FavouriteCocktailsScreen(
+        AsyncCocktailRepository(),
+      ),
+    );
+  }
+}
+
+/* favourite_cocktails_screen уже готов. Остается только сделать переход со списка коктейлей 
+на страницу коктейля. Основная загвоздка в том что в списке у нас модель CocktailDefinition, 
+а на странице CocktailDetailsScreen мы используем модель Cocktail.
+Эту проблему вам и предлагается решить.
+Получить модель по id можно с помощью AsyncCocktailRepository().fetchCocktailDetails(id) 
+
+
+
+Потребуется: реализовать навигацию в экран описания коктейля из списка Избранное (Favourite Cocktails).
+
+- Склонировать соотвествующий github репозиторий с заготовкой проекта для этого урока
+- Открыть класс экрана FavouriteCocktailsScreen
+- Внести изменения в код экрана, выделив соотвествующие обработчики событий на нажатие элемента 
+из списка избранных коктейлей
+- В обработчике события на переход к экрану описания коктейля выполнить асинхронное получение 
+модели коктейля
+- В обработчике события на переход к экрану описания коктейля после получения модели коктейля 
+выполнить навигацию к экрану описания коктейля (CocktailDetailsScreen) с передачей полученной модели
+- Убедиться, что CocktailDetailsScreen отображает корректно полученную модель
+
+*/
