41e6061e46a862e9c341e36a114e5423310ac7e7
glaschenko <a.glaschenko@gmail.com>
2021-01-08 19:05:24 +0400

    Lesson 12 homework Glaschenko - part 1, animated "Favourite" icon

--- lesson_14/homework/lib/ui/cocktaildetails/cocktail_details.dart
+++ lesson_14/homework/lib/ui/cocktaildetails/cocktail_details.dart
@@ -24,7 +24,7 @@ class CocktailDetails extends StatelessWidget {
                           fontFamily: fontFamily))),
               Container(
                   padding: EdgeInsets.only(right: 24),
-                  child: Icon(Icons.favorite, color: Colors.white, size: 20))
+                  child: AnimatedFavourite())
             ]),
             SizedBox(height: 10),
             Text("id:${cocktail.id}",
@@ -65,5 +65,68 @@ class CocktailDetails extends StatelessWidget {
             style: TextStyle(
                 color: Colors.white, fontSize: 13, fontFamily: fontFamily))));
     return res;
+  }
+
+
+}
+
+class AnimatedFavourite extends StatefulWidget{
+
+  @override
+  _AnimatedFavouriteState createState() => _AnimatedFavouriteState();
+}
+
+class _AnimatedFavouriteState extends State<AnimatedFavourite> with TickerProviderStateMixin{
+  AnimationController _controller;
+  Animation<double> _animation;
+  bool _selected = true;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync:  this , duration:  new Duration(milliseconds: 300));
+    _animation = TweenSequence<double>([
+      TweenSequenceItem<double>(tween: Tween<double>(begin: 20.0, end: 28.0), weight: 30),
+      TweenSequenceItem<double>(tween: Tween<double>(begin: 30.0, end: 25.0), weight: 20),
+      TweenSequenceItem<double>(tween: Tween<double>(begin: 25.0, end: 28.0), weight: 20),
+      TweenSequenceItem<double>(tween: Tween<double>(begin: 30.0, end: 20.0), weight: 30),
+    ]).animate(_controller);
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.addStatusListener(_handleAnimationStatusChange);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+      return GestureDetector(
+          child: Stack(children: <Widget>[
+            Container(width: 29, height: 29, color: Color(0x00000000),),
+            Icon(Icons.favorite, color: Colors.white, size: _animation.value),
+            if(!_selected) Positioned(
+                left: 2, top: 2,
+                child: Icon(Icons.favorite, color: Color(0xFF1A1927), size: _animation.value-4)
+            ),
+          ]),
+          onTap: () {
+            _controller.forward();
+          },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super .dispose();
+  }
+
+  void _handleAnimationStatusChange(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      _controller.reset();
+      setState(() => _selected = !_selected);
+    }
   }
 }
